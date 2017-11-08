0144b942ce38731b5ff346c398b554f8615818f6
VanMinh <vanminh26.97@gmail.com>
2017-11-08 19:04:48 +0700

    loz

--- ChemistryApp/src/process/phanBiet.java
+++ ChemistryApp/src/process/phanBiet.java
@@ -30,7 +30,64 @@ public class phanBiet {
     }    
     
     public static boolean coKhi(List <String> G){
-        
+        for (int i=0; i<G.size(); i++){
+            String v = G.get(i);
+            if (pu.khi.containsKey(v)){
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    public static void Show(List<HopChat> G, HopChat A){
+        for (int i=0; i<G.size(); i++){
+            HopChat v = G.get(i);
+            List <String> phanUng = pu.pu(v, A);
+            if (phanUng.size() == 0){
+                phanUng = pu.pu(A, v);
+            }
+            // Không có hiện tượng
+            boolean x = coKetTua(phanUng);
+            boolean y = coKhi(phanUng);
+            boolean z = !x && !y;
+            if (phanUng.size() == 0){                
+                System.out.println(v.getCTHH() + " không phản ứng " + A.getCTHH() + " ==> không thấy hiện tượng");           
+            }            
+            else{
+                if (z){                    
+                    System.out.print(v.getCTHH() + " + " + A.getCTHH() + " = ");
+                    for (int j=0; j<phanUng.size() - 1; j++){
+                        System.out.print(phanUng.get(j) + " + ");
+                    }
+                    System.out.println(phanUng.get(phanUng.size() - 1) + " ==> không thấy hiện tượng");
+                }
+                else{
+                    if (x && !y){
+                        System.out.print(v.getCTHH() + " + " + A.getCTHH() + " = ");
+                        for (int j=0; j<phanUng.size() - 1; j++){
+                            System.out.print(phanUng.get(j) + " + ");
+                        }
+                        System.out.println(phanUng.get(phanUng.size() - 1) + " ==> kết tủa");
+                    }
+                    else{
+                        if (!x && y){
+                            System.out.print(v.getCTHH() + " + " + A.getCTHH() + " = ");
+                            for (int j=0; j<phanUng.size() - 1; j++){
+                                System.out.print(phanUng.get(j) + " + ");
+                            }
+                            System.out.println(phanUng.get(phanUng.size() - 1) + " ==> khí");
+                        }
+                        else{
+                            System.out.print(v.getCTHH() + " + " + A.getCTHH() + " = ");
+                            for (int j=0; j<phanUng.size() - 1; j++){
+                                System.out.print(phanUng.get(j) + " + ");
+                            }
+                            System.out.println(phanUng.get(phanUng.size() - 1) + " ==> vừa khí vừa kết tủa");
+                        }
+                    }
+                }
+            }
+        }
     }
     
     public static List<List<HopChat>> Collect(List<HopChat> G, HopChat A){        
@@ -42,9 +99,12 @@ public class phanBiet {
         for (int i=0; i<G.size(); i++){
             HopChat v = G.get(i);
             List <String> phanUng = pu.pu(v, A);
+            if (phanUng.size() == 0){
+                phanUng = pu.pu(A, v);
+            }
             // Không có hiện tượng
-            boolean x = coKetTua(v);
-            boolean y = coKhi(v);
+            boolean x = coKetTua(phanUng);
+            boolean y = coKhi(phanUng);
             boolean z = !x && !y;
             if (phanUng.size() == 0 || z){
                 X.add(v);
@@ -76,15 +136,16 @@ public class phanBiet {
     public static void phanBiet(List <HopChat> G){
         if (G.size() == 0) return;
         if (G.size() == 1) {
-            System.out.println("Đã phân biệt được " + G.get(0).getCTHH() + "\n");
+            System.out.println("Đã phân biệt được " + G.get(0).getCTHH());
             return ;
         }
         // Luật quỳ tím
         List <HopChat> X = new Vector<HopChat>();
         List <HopChat> Y = new Vector<HopChat>();   
         List <HopChat> Z = new Vector<HopChat>();
+        List <HopChat> T = new Vector<HopChat>();
         for (int i=0; i<G.size(); i++){
-            HopChat v = G.get(i);
+            HopChat v = G.get(i);            
             if (axit(v)){
                 X.add(v);
             }
@@ -97,6 +158,22 @@ public class phanBiet {
         }
         int cnt = (X.size() >= 1 ? 1 : 0) + (Y.size() >= 1 ? 1 : 0) + (Z.size() >= 1 ? 1 : 0);
         if (cnt > 1){
+            System.out.println("Chúng ta dùng quỳ tím để chia làm " + cnt + " nhóm chất: ");
+            if (X.size() > 0){
+                System.out.print("Làm quỳ tím hóa đỏ gồm những chất: ");
+                for (int i=0; i<X.size(); i++) System.out.print(X.get(i).getCTHH() + " ");
+                System.out.println();
+            }
+            if (Y.size() > 0){
+                System.out.print("Làm quỳ tím hóa xanh gồm những chất: ");
+                for (int i=0; i<Y.size(); i++) System.out.print(Y.get(i).getCTHH() + " ");
+                System.out.println();
+            }
+            if (Z.size() > 0){
+                System.out.print("Làm quỳ tím không đổi màu gồm những chất: ");
+                for (int i=0; i<Z.size(); i++) System.out.print(Z.get(i).getCTHH() + " ");
+                System.out.println();
+            }
             phanBiet(X);
             phanBiet(Y);
             phanBiet(Z);
@@ -112,17 +189,43 @@ public class phanBiet {
                 
                 HopChat any = new HopChat(c, a);
                 
-                List <List<HopChat> > L = Collect(G, any);
-                X = L.get(0);   Y = L.get(1);   Z = L.get(2);
+                List <List<HopChat> > L = Collect(G, any);                               
                 
-                cnt = (X.size() >= 1 ? 1 : 0) + (Y.size() >= 1 ? 1 : 0) + (Z.size() >= 1 ? 1 : 0);
-                if (cnt > 1){
+                X = L.get(0);   Y = L.get(1);   Z = L.get(2);   T = L.get(3);
+                
+                cnt = (X.size() >= 1 ? 1 : 0) + (Y.size() >= 1 ? 1 : 0) + (Z.size() >= 1 ? 1 : 0) + (T.size() >= 1 ? 1 : 0);
+                if (cnt > 1){                                                           
+                    System.out.println("Chúng ta dùng chất " + any.getCTHH() + " để chia làm " + cnt + " nhóm");
+                    if (X.size() > 0){
+                        System.out.print("Phản ứng không có hiện tượng gồm những chất: ");
+                        for (int i=0; i<X.size(); i++) System.out.print(X.get(i).getCTHH() + " ");
+                        System.out.println();
+                    }
+                    if (Y.size() > 0){
+                        System.out.print("Phản ứng tạo kết tủa gồm những chất: ");
+                        for (int i=0; i<Y.size(); i++) System.out.print(Y.get(i).getCTHH() + " ");
+                        System.out.println();
+                    }
+                    if (T.size() > 0){
+                        System.out.print("Phản ứng tạo khí gồm những chất: ");
+                        for (int i=0; i<Z.size(); i++) System.out.print(Z.get(i).getCTHH() + " ");
+                        System.out.println();
+                    }
+                    if (T.size() > 0){
+                        System.out.print("Phản ứng vừa tạo khí vừa tạo kết tủa gồm những chất: ");
+                        for (int i=0; i<T.size(); i++) System.out.print(T.get(i).getCTHH() + " ");
+                        System.out.println();
+                    }
+                    
+                    Show(G, any);
+                    
                     phanBiet(X);
                     phanBiet(Y);
                     phanBiet(Z);
+                    phanBiet(T);
                     return ;
                 }
             }
         }
-    }
+    } 
 }
